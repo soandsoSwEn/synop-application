941d370ec6512691f2eb459681299ca069a9b30b
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2021-04-05 14:57:28 +0300

    added synop-application.php file

---
+++ synop-application.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: Synop Application
+ * Description: Plugin for testing and approbation of the soandso/synop vendor library
+ * Plugin URI: https://soandso.biz/
+ * Author: Author
+ * Author URI: https://soandso.biz/
+ * Version: 0.0.1
+ * License: GPL2
+ * Text Domain: synop-application
+ */
+
+/*
+    Copyright (C) Year  Author  Email
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+namespace Synop_Application;
