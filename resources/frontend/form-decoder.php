5e5a8a6dc7997e6c38ac152a0ebbf5911cb8df2c
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2021-04-06 21:06:52 +0300

    added resorce for form and decode result of decoder

---
+++ resources/frontend/form-decoder.php
@@ -0,0 +1,16 @@
+<div class="row">
+    <div class="col-md-12">
+        <form action="#" method="post">
+            <div class="row">
+                <div class="col-md-8">
+                    <input type="text" id="reportData" name="reportData" placeholder="Weather Report" style="width: 100%">
+                </div>
+                <div class="col-md-4">
+                    <button type="submit" id="submitDecoder" class="pull-left">DECODER_TITLE</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<div id="resultDecode"></div>
